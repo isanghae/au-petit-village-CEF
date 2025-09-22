1d947e1aed70ac474a4992ac33dfd272325d434f
AnaisDijoux <thesiananana@gmail.com>
2025-09-22 17:26:18 +0200

    Fin du débogage et de la mise en forme CSS

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -7,7 +7,7 @@ export class ProductService {
 
   FigurineAsterix: string = 'assets/images/figurine-asterix-le-gaulois.jpg';
   FigurineObelix: string = 'assets/images/figurine-obélix-le-gaulois.png';
-  FigurineIdefix: string = 'assets/images/figurine-idefix-le-gaulois.png';
+  FigurineIdefix: string = 'assets/images/figurine-idefix.png';
 
   private products = [
     {
@@ -15,7 +15,7 @@ export class ProductService {
       name: 'Astérix Le Gaulois',
       image: this.FigurineAsterix,
       price: 24.99,
-      description: "Figurine en résine de haute qualité représentant le célèbre héros gaulois, Astérix. Il est vétu de sa tenue traditionelle, coiffé de son casque ailé, et tient son éternelle potion magique dans une main. Cette figurine est un hommage parfait au personnage d'Astérix créé par René Goscinny et Albert Uderzo.",
+      description: "Figurine en résine de haute qualité représentant le célèbre héros gaulois, Astérix. Il est vétu de sa tenue traditionelle, coiffé de son casque ailé, et arborant son éternelle potion magique à la ceinture. Cette figurine est un hommage parfait au personnage d'Astérix créé par René Goscinny et Albert Uderzo.",
       materiau: 'Résine',
       hauteur: '15 cm',
       disponibilite: 'En stock'
@@ -25,7 +25,7 @@ export class ProductService {
       name: 'Obélix Le Gaulois',
       image: this.FigurineObelix,
       price: 26.99,
-      description: "Figurine en résine d'Obélix, le fidèle ami d'Astérix. Obélix est vêtu de sa tenue caractéristique avec son short rayé et son casque, et il tient un menhir dans une main. Cette figurine est parfaite pour les collectionneurs et les fans d'Astérix et Obélix.",
+      description: "Figurine en résine d'Obélix, le fidèle ami d'Astérix. Obélix est vêtu de sa tenue caractéristique avec son short rayé et son casque. Cette figurine est parfaite pour les collectionneurs et les fans d'Astérix et Obélix.",
       materiau: 'Résine',
       hauteur: '15 cm',
       disponibilite: 'En stock'
@@ -35,7 +35,7 @@ export class ProductService {
       name: 'Idéfix Le Chien',
       image: this.FigurineIdefix,
       price: 12.99,
-      description: "Figurine en plastique de haute qualité représentant Idéfix, le petit chien blanc d'Astérix et Obélix. Idéfix est assis paisiblement avec son os à mâcher préféré à ses côtés. Une figurine parfaite pour les enfants et les fans de tous âges.",
+      description: "Figurine en plastique de haute qualité représentant Idéfix, le petit chien blanc d'Astérix et Obélix. Idéfix est assis paisiblement et attend sagement ses fidèles amis. Une figurine parfaite pour les enfants et les fans de tous âges.",
       materiau: 'Plastique',
       hauteur: '7 cm',
       disponibilite: 'En stock'
